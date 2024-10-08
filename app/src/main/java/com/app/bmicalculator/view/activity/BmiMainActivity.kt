ad3694c1416c2c5ca729489a4bf82de26bc36b61
Akshay Khajuria <akshaykhajuria92@gmail.com>
2024-10-08 15:32:02 +0530

    update backstack

--- app/src/main/java/com/app/bmicalculator/view/activity/BmiMainActivity.kt
+++ app/src/main/java/com/app/bmicalculator/view/activity/BmiMainActivity.kt
@@ -29,9 +29,9 @@ class BmiMainActivity : BaseActivity() {
                     supportFragmentManager.popBackStack()
                 }
 
-                else -> {
+                /*else -> {
                     gotoHomeScreen()
-                }
+                }*/
             }
         }
 
